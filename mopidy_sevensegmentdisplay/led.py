530c27c2acd71098ada4997969aeec8739ff1cb2
JuMalIO <spamjulius@mail.com>
2024-02-05 00:08:25 +0200

    led

--- mopidy_sevensegmentdisplay/led.py
+++ mopidy_sevensegmentdisplay/led.py
@@ -1,5 +1,6 @@
 import colorsys
 import random
+import logging
 import RPi.GPIO as GPIO
 from .lib_nrf24 import NRF24
 
@@ -15,7 +16,7 @@ class Led:
 
         GPIO.setmode(GPIO.BCM)
 
-        pipe = [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]
+        pipes = [[0xE0, 0xE0, 0xF1, 0xF1, 0xE0], [0xF1, 0xF1, 0xF0, 0xF0, 0xE0]]
 
         import spidev
         self._spi = spidev.SpiDev()
@@ -26,12 +27,13 @@ class Led:
 
         self._radio = NRF24(GPIO, self._spi)
         self._radio.begin(1, 25)
-        self._radio.setPayloadSize(32)
+        self._radio.setPayloadSize(8)
         self._radio.setChannel(0x7A)
         self._radio.setDataRate(NRF24.BR_1MBPS)
         self._radio.setPALevel(NRF24.PA_MIN)
-        self._radio.setAutoAck(False)
-        self._radio.openWritingPipe(pipe)
+        self._radio.setAutoAck(True)
+        self._radio.openWritingPipe(pipes[0])
+        self._radio.openReadingPipe(1, pipes[1])
         self._radio.printDetails()
         
     def setColor(self, hue, sat = 1, val = 1):
@@ -41,6 +43,15 @@ class Led:
         c = colorsys.hsv_to_rgb(hue / 360.0, sat, val)
             
         self._radio.write([0, int(c[0] * 255), int(c[1] * 255), int(c[2] * 255)])
+
+        if self._radio.isAckPayloadAvailable():
+            buffer = []
+            self._radio.read(buffer, self._radio.getDynamicPayloadSize())
+            logging.info("NRF24 ACK Received:"),
+            logging.info(buffer)
+        else:
+            logging.info("Received: Ack only, no payload")
+
 
     def setRandomColor(self):
         self.setColor(random.random() * 360)
