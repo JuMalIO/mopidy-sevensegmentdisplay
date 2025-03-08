cdb5dd2c88e53a3bf9073fc8ae11038cf75eb48b
JuMalIO <spamjulius@mail.com>
2025-03-08 13:08:53 +0200

    button debounce

--- mopidy_sevensegmentdisplay/gpio.py
+++ mopidy_sevensegmentdisplay/gpio.py
@@ -15,21 +15,31 @@ class Gpio:
         self._lock = threading.Lock()
 
         if (buttons_enabled):
-            b1 = Button(self.POWER_BUTTON_PIN)
-            b2 = Button(self.MENU_BUTTON_PIN)
-            b3 = Button(self.LEFT_BUTTON_PIN)
-            b4 = Button(self.RIGHT_BUTTON_PIN)
+            self.b1 = Button(self.POWER_BUTTON_PIN)
+            self.b2 = Button(self.MENU_BUTTON_PIN)
+            self.b3 = Button(self.LEFT_BUTTON_PIN)
+            self.b4 = Button(self.RIGHT_BUTTON_PIN)
 
-            b1.when_pressed = lambda gpio: on_power() if not self._lock.locked() else None
-            b2.when_pressed = lambda gpio: on_menu() if not self._lock.locked() else None
-            b3.when_pressed = lambda gpio: on_left() if not self._lock.locked() else None
-            b4.when_pressed = lambda gpio: on_right() if not self._lock.locked() else None
+            self.b1.when_pressed = self.debounce(lambda gpio: on_power() if not self._lock.locked() else None)
+            self.b2.when_pressed = self.debounce(lambda gpio: on_menu() if not self._lock.locked() else None)
+            self.b3.when_pressed = self.debounce(lambda gpio: on_left() if not self._lock.locked() else None)
+            self.b4.when_pressed = self.debounce(lambda gpio: on_right() if not self._lock.locked() else None)
 
         if (relay_enabled):
             self._is_relay_on = False
             self.relay = LED(self.RELAY_PIN)
 
         self._relay_enabled = relay_enabled
+
+    def debounce(self, callback, wait=0.05):
+        last_called = [0]
+
+        def wrapper():
+            if (time.time() - last_called[0] > wait):
+                callback()
+                last_called[0] = time.time()
+
+        return wrapper
 
     def switch_relay(self, value):
         if (self._relay_enabled and self._lock.acquire()):
